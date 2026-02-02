03142ebc50f61bebb80b74ec8719a4b8b72ff2fc
Keerthana R <keerthana241495@gmail.com>
2026-02-02 17:00:46 +0530

    ugh

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -9,44 +9,32 @@ export default function useAuth(requiredRole = null) {
   const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
   useEffect(() => {
-    let ismounted = true;
     const checkAuth = async () => {
       try {
         const res = await fetch(`${BASE_URL}/api/user`, {
           credentials: "include",
         });
 
-        if (!res.ok) throw new Error("Not Authenticated!");
+        if (!res.ok) throw new Error("Unauthenticated");
 
         const data = await res.json();
-        console.log(data);
 
-        if (requiredRole) {
-          if (!data.role) {
-            console.error("Role missing from /api/user response", data);
-            router.replace("/login");
-            return;
-          }
-
-          if (data.role !== requiredRole) {
-            router.replace("/login");
-            return;
-          }
-        }
-
-        if (ismounted) {
-          setUser(data);
-          setLoading(false);
-        }
-      } catch {
-        if (ismounted) {
-          setLoading(false);
+        if (requiredRole && data.role !== requiredRole) {
           router.replace("/login");
+          return;
         }
+
+        setUser(data);
+      } catch {
+        console.log("AUTH FAIL", { requiredRole, data });
+        router.replace("/login");
+      } finally {
+        setLoading(false);
       }
     };
 
     checkAuth();
-  }, [router, requiredRole]);
+  }, []); 
+
   return { user, loading };
 }
